814f4caba7407358d4b702cdc4d1fb9fed7afeae
vitiazolha <vitiazolha@gmail.com>
2016-09-30 13:24:46 +0300

    added new file for game

--- uno/uno.go
+++ uno/uno.go
@@ -2,23 +2,10 @@ package uno
 
 import (
 	"fmt"
-	"time"
 	"os"
 	"github.com/nsf/termbox-go"
 	)
-var playersCounter = 2
 
-func NewGame() {
-	//deck := newCardDeck()
-	players := make([][]Card, playersCounter) 
-
-	for index, _ := range players {
-		players[index] = make([]Card, 0, 10)
-	}
-
-	time.Sleep(1000 * time.Millisecond)
-	fmt.Println("This is new game")
-}
 
 func Help() {
 	fmt.Println("This is help")
