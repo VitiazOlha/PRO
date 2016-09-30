db9e200c04c93cd1c3428cb184ec76d7108dfa59
BorisTopchiev <topcievboris97@gmail.com>
2016-09-30 14:49:43 +0300

    JSON parsing methods

--- uno/card.go
+++ uno/card.go
@@ -4,6 +4,7 @@ type CardDeck [] Card
 
 const width  = 4
 const height = 5
+// Provides a general Drawable to be rendered.
 
 type Card struct {
 	color string
--- uno/game.go
+++ uno/game.go
@@ -2,12 +2,15 @@ package uno
 
 import (
 	"fmt"
+	"encoding/json"
+	"io/ioutil"
 	)
 
 
 var playersCounter = 2
 
 func NewGame() {
+
 	deck := newCardDeck()
 	players := make([][]Card, playersCounter) 
 
@@ -21,4 +24,50 @@ func NewGame() {
 
 func newCardDeck() {
 	
+	ldeck, err := ioutil.ReadFile("deck.json")
+	checkErr(err)
+	parsers := make(map[string]tl.EntityParser)
+	err = LoadDeckFromJson(string(ldeck), parsers)
+	checkErr(err)
 }
+
+
+func LoadDeckFromJson(jsonMap string, parsers map[string]EntityParser) error {
+	
+	var deck CardDeck
+	parsedMap := []levelMap{}
+	if err := json.Unmarshal([]byte(jsonMap), &parsedMap); err != nil {
+		return err
+	}
+	for _, lm := range parsedMap {
+		
+		var card Card 
+		for i := 0; i < int(data["quantity"].(int)); i++ {
+			card = parseCard(lm.Data)
+			deck = append(deck, card)
+		}
+	}
+	return nil
+}
+
+func parseCard(data map[string]interface{}) *Card{
+	return NewCard(
+		data["color"].(string)),
+		data["value"].(string)),
+		int(data["cost"].(int)),
+		data["f"].(string)),
+	)
+}
+
+
+func parseText(data map[string]interface{}) *Text {
+	return NewText(
+		int(data["x"].(float64)),
+		int(data["y"].(float64)),
+		data["text"].(string),
+		Attr(data["fg"].(float64)),
+		Attr(data["bg"].(float64)),
+	)
+}
+
+
